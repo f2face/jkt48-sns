56e95646d4c24650a5375e79f707190e104ecd02
Surya Oktafendri <f2face@f2face.com>
2024-01-29 13:45:38 +0700

    Refactor

---
+++ src/accounts.ts
@@ -0,0 +1,75 @@
+export const officials = [
+    // Official
+    'jkt48',
+
+    // Merch
+    'jkt48-merch',
+
+    // Moshi2
+    'jkt48-cs',
+] as const;
+
+export const members = [
+    // Members
+    'adel',
+    'amanda',
+    'ashel',
+    'callie',
+    'christy',
+    'eli',
+    'ella',
+    'feni',
+    'fiony',
+    'flora',
+    'freya',
+    'gita',
+    'gracia',
+    'indah',
+    'indira',
+    'jessi',
+    'kathrina',
+    'lia',
+    'lulu',
+    'lyn',
+    'marsha',
+    'muthe',
+    'olla',
+    'oniel',
+    'raisha',
+    'shani',
+    'zee',
+
+    // Gen 11
+    'alya',
+    'anindya',
+    'cathy',
+    'chelsea',
+    'cynthia',
+    'daisy',
+    'danella',
+    'elin',
+    'gendis',
+    'gracie',
+    'greesel',
+    'jeane',
+    'michie',
+
+    // Gen 12
+    'aralie',
+    'delynn',
+    'erine',
+    'fritzy',
+    'kimmy',
+    'lana',
+    'levi',
+    'lily',
+    'moreen',
+    'nachia',
+    'nala',
+    'nayla',
+    'oline',
+    'regie',
+    'ribka',
+    'shasa',
+    'trisha',
+] as const;
