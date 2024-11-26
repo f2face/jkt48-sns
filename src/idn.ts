5c7c490ae3172d5afbd7607d7470db28cbd9d613
Surya Oktafendri <f2face@f2face.com>
2024-11-26 15:42:29 +0700

    [IDN] Update `defaultStreamUrl`s

--- src/idn.ts
+++ src/idn.ts
@@ -16,10 +16,10 @@ export const officialsIdn: Record<ActiveOfficialsIdn, Readonly<IdnAccount>> = {
         username: 'jkt48-official',
         uuid: '50a27780-93e7-4e40-8474-60f6e0cca6da',
         name: 'JKT48',
-        /* defaultStreamUrl:
-            'https://4b964ca68cf1.us-east-1.playback.live-video.net/api/video/v1/us-east-1.050891932989.channel.4idUspKIZskG.m3u8', */
         defaultStreamUrl:
-            'https://4b964ca68cf1.us-east-1.playback.live-video.net/api/video/v1/us-east-1.050891932989.channel.1s3sSrncQ8Ch.m3u8',
+            'https://4b964ca68cf1.us-east-1.playback.live-video.net/api/video/v1/us-east-1.050891932989.channel.4idUspKIZskG.m3u8',
+        /* defaultStreamUrl:
+            'https://4b964ca68cf1.us-east-1.playback.live-video.net/api/video/v1/us-east-1.050891932989.channel.1s3sSrncQ8Ch.m3u8', */
     },
     //#endregion
 } as const;
@@ -184,7 +184,8 @@ export const membersIdn: Record<ActiveMembersIdn, Readonly<IdnAccount>> = {
         username: 'jkt48_raisha',
         uuid: '728d224e-6ccb-43d2-a029-a0147dc7fdee',
         name: 'Raisha',
-        defaultStreamUrl: undefined,
+        defaultStreamUrl:
+            'https://4b964ca68cf1.us-east-1.playback.live-video.net/api/video/v1/us-east-1.050891932989.channel.rjVZYmbffgGE.m3u8',
     },
     //#endregion
 
